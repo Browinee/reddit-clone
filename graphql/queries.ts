6360d7926e4c029be89a68a85bcaa56311187488
justin <>
2022-06-20 17:09:15 +0800

    feat: add feed and post ui

--- graphql/queries.ts
+++ graphql/queries.ts
@@ -1,5 +1,38 @@
 import { gql } from "@apollo/client";
 
+export const GET_ALL_POSTS = gql`
+  query getAllPosts {
+    getPostList {
+      body
+      created_at
+      id
+      image
+      title
+      subreddit_id
+      username
+      commentList {
+        id
+        created_at
+        post_id
+        text
+        username
+      }
+      subreddit {
+        id
+        topic
+        created_at
+      }
+      voteList {
+        created_at
+        id
+        post_id
+        upvote
+        username
+      }
+    }
+  }
+`;
+
 export const GET_SUBREDDIT_LIST_BY_TOPIC = gql`
   query getSubredditListByTopic($topic: String!) {
     getSubredditListByTopic(topic: $topic) {
---
+++ typings.d.ts
@@ -0,0 +1,36 @@
+interface Post {}
+
+interface Comments {
+  created_at: string;
+  id: number;
+  post_id: number;
+  text: string;
+  username: string;
+}
+
+interface Vote {
+  created_at: string;
+  id: number;
+  post_id: number;
+  upvote: boolean;
+  username: string;
+}
+
+interface Subreddit {
+  created_at: string;
+  id: number;
+  topic: string;
+}
+
+interface Post {
+  body: string;
+  created_at: string;
+  id: number;
+  image: string;
+  subreddit_id: number;
+  title: string;
+  username: string;
+  voteList: Vote[];
+  commentList: Comments[];
+  subreddit: Subreddit[];
+}
